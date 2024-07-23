e04549173f23f28cda0d0613262dbf49421b53fb
Muhammad Arslan <106810978+iarslanch@users.noreply.github.com>
2024-07-23 18:40:31 +0500

    push

---
+++ modules/arc/variables.tf
@@ -0,0 +1,9 @@
+variable "github_token" {
+  description = "GitHub Personal Access Token"
+  type        = string
+}
+
+variable "repository" {
+  description = "GitHub repository to run actions"
+  type        = string
+}
