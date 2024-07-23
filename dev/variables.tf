fb1407178e4dc3e54d830569f2bc6faf64f6d3e5
Muhammad Arslan <106810978+iarslanch@users.noreply.github.com>
2024-07-23 22:48:59 +0500

    destroy

--- dev/variables.tf
+++ dev/variables.tf
@@ -20,16 +20,6 @@ variable "project" {
 
 }
 
-variable "github_token" {
-  description = "GitHub Personal Access Token"
-  type        = string
-}
-
-variable "repository" {
-  description = "GitHub repository to run actions"
-  type        = string
-}
-
 
 
 
