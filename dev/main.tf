a9e161fe3db01ea670b9707ab200cac697f1c73a
Muhammad Arslan <106810978+iarslanch@users.noreply.github.com>
2024-07-25 01:11:28 +0500

    push code

--- dev/main.tf
+++ dev/main.tf
@@ -129,12 +129,3 @@ module "arc" {
   github_token = var.github_token
 }
 
-data "aws_eks_cluster" "this" {
-  name = var.cluster_name
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = var.cluster_name
-}
-
-
