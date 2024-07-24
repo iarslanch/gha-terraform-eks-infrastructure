eaffc55cba53db064ad8ad8da4814ea33dbf3598
Muhammad Arslan <106810978+iarslanch@users.noreply.github.com>
2024-07-25 01:16:14 +0500

    update provider code

--- dev/provider.tf
+++ dev/provider.tf
@@ -1,6 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
-  #profile = "default"
 }
 
 provider "helm" {
