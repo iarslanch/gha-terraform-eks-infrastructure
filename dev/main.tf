ceec0b85d9a8d9d626aea3d05455566549b4d152
Muhammad Arslan <106810978+iarslanch@users.noreply.github.com>
2024-07-25 02:35:37 +0500

    updated

--- dev/main.tf
+++ dev/main.tf
@@ -3,21 +3,6 @@ provider "aws" {
   region = var.region
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
-}
-
-provider "kubectl" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-  load_config_file       = false
-}
-
 locals {
   cluster_name = "${var.project}-cluster-${var.env}"
 }
