cca5c57f0012072e7e2d21a0ea3b787abf93b10b
Muhammad Arslan <106810978+iarslanch@users.noreply.github.com>
2024-07-25 02:07:48 +0500

    update

--- modules/eks-cluster/08-sg.tf
+++ modules/eks-cluster/08-sg.tf
@@ -4,9 +4,10 @@ resource "aws_security_group_rule" "tcp" {
   from_port         = tonumber(var.tcp_ports[count.index])
   to_port           = tonumber(var.tcp_ports[count.index])
   protocol          = "tcp"
-  cidr_blocks       = [lookup(var.cidrs, count.index, "0.0.0.0/0")] # Default to "0.0.0.0/0" if not enough CIDRs
+  cidr_blocks       = count.index < length(var.cidrs) ? [var.cidrs[count.index]] : ["0.0.0.0/0"]
   security_group_id = aws_security_group.default.id
 }
+
 
 
 resource "aws_security_group_rule" "udp" {
@@ -15,7 +16,7 @@ resource "aws_security_group_rule" "udp" {
   from_port         = tonumber(var.udp_ports[count.index])
   to_port           = tonumber(var.udp_ports[count.index])
   protocol          = "udp"
-  cidr_blocks       = [var.cidrs[count.index]]
+  cidr_blocks       = count.index < length(var.cidrs) ? [var.cidrs[count.index]] : ["0.0.0.0/0"]
   security_group_id = aws_security_group.default.id
 }
 
