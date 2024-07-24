5349a57043ef277bd6df0e579bd1e99f9c006017
Muhammad Arslan <106810978+iarslanch@users.noreply.github.com>
2024-07-25 03:23:58 +0500

    push

--- dev/main.tf
+++ dev/main.tf
@@ -5,6 +5,14 @@ provider "aws" {
 locals {
   cluster_name = "${var.project}-cluster-${var.env}"
 }
+
+data "aws_eks_cluster" "this" {
+  name = local.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = data.aws_eks_cluster.this.name
+}
 module "network" {
   source                          = "../modules/network"
   vpc_cidr                        = "72.11.0.0/20"
--- modules/eks-cluster/data_source.tf
+++ modules/eks-cluster/data_source.tf
@@ -1,3 +1,7 @@
+locals {
+  cluster_name = "${var.project}-cluster-${var.env}"
+}
+
 data "aws_eks_cluster" "this" {
   name = local.cluster_name
 }
