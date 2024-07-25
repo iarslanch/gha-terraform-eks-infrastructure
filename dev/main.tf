a69392e79c8f38f8e85df417e149ec3098065aa3
Muhammad Arslan <106810978+iarslanch@users.noreply.github.com>
2024-07-25 11:08:48 +0500

    updated

--- dev/main.tf
+++ dev/main.tf
@@ -134,3 +134,14 @@ module "arc" {
   cluster_token         = data.aws_eks_cluster_auth.this.token
 }
 
+locals {
+  cluster_name = "${var.project}-cluster-${var.env}"
+}
+
+data "aws_eks_cluster" "this" {
+  name = local.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = data.aws_eks_cluster.this.name
+}
