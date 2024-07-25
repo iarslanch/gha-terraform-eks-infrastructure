7394118540f5c1a3e078badbe8692f3184e0be1c
Muhammad Arslan <106810978+iarslanch@users.noreply.github.com>
2024-07-25 12:34:01 +0500

    push code

--- modules/arc/main.tf
+++ modules/arc/main.tf
@@ -4,24 +4,24 @@ provider "kubernetes" {
   token                  = var.cluster_token
 }
 
-# Add the Helm repository for actions-runner-controller
-resource "null_resource" "add_actions_runner_repo" {
+# Add the Actions Runner Controller Helm repository
+resource "null_resource" "add_helm_repo" {
   provisioner "local-exec" {
-    command = <<-EOT
-      helm repo add actions-runner-controller https://actions-runner-controller.github.io/actions-runner-controller
+    command = <<EOT
+      helm repo add actions-runner-controller https://actions-runner-controller.github.io/actions-runner-controller && \
       helm repo update
     EOT
   }
 }
 
-# Install the Actions Runner Controller CRDs and Helm chart
+# Install the Actions Runner Controller using Helm
 resource "helm_release" "actions_runner_controller" {
-  depends_on = [null_resource.add_actions_runner_repo]
+  depends_on = [null_resource.add_helm_repo]
 
-  name             = "actions-runner-controller"
-  repository       = "https://actions-runner-controller.github.io/actions-runner-controller"
-  chart            = "actions-runner-controller"
-  namespace        = "actions-runner-system"
+  name       = "actions-runner-controller"
+  repository = "https://actions-runner-controller.github.io/actions-runner-controller"
+  chart      = "actions-runner-controller"
+  namespace  = "actions-runner-system"
   create_namespace = true
 
   set {
@@ -40,8 +40,17 @@ resource "helm_release" "actions_runner_controller" {
   }
 }
 
-resource "kubernetes_manifest" "runner_deployment" {
+# Wait for the Actions Runner Controller CRDs to register
+resource "null_resource" "wait_for_crd_registration" {
+  provisioner "local-exec" {
+    command = "sleep 30"
+  }
   depends_on = [helm_release.actions_runner_controller]
+}
+
+# Apply the RunnerDeployment resource
+resource "kubernetes_manifest" "runner_deployment" {
+  depends_on = [null_resource.wait_for_crd_registration]
 
   manifest = {
     apiVersion = "actions.summerwind.dev/v1alpha1"
@@ -62,3 +71,25 @@ resource "kubernetes_manifest" "runner_deployment" {
   }
 }
 
+# Variables for Kubernetes provider
+variable "cluster_endpoint" {
+  description = "The endpoint of the Kubernetes cluster"
+  type        = string
+}
+
+variable "cluster_ca_certificate" {
+  description = "The base64 encoded certificate for the Kubernetes cluster"
+  type        = string
+}
+
+variable "cluster_token" {
+  description = "The token for the Kubernetes cluster"
+  type        = string
+}
+
+# Variable for GitHub token
+variable "github_token" {
+  description = "GitHub token with permissions to manage runners"
+  type        = string
+  sensitive   = true
+}
